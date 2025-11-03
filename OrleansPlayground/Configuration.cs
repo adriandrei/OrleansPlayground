5dd4dccbe0e4fbff1964c0cec36723e44d770237
Adrian Andrei <aandrei@uniphar.ie>
2025-11-03 13:14:06 +0200

    pre-presentation values.

--- OrleansPlayground/Configuration.cs
+++ OrleansPlayground/Configuration.cs
@@ -1,7 +1,7 @@
 public static class Configuration
 {
     public static TimeSpan ReminderDue = TimeSpan.FromSeconds(5);
-    public static TimeSpan ReminderPeriod = TimeSpan.FromSeconds(30);
+    public static TimeSpan ReminderPeriod = TimeSpan.FromSeconds(20);
     public static TimeSpan TimerDue = TimeSpan.FromSeconds(5);
     public static TimeSpan TimerPeriod = TimeSpan.FromSeconds(10);
 
