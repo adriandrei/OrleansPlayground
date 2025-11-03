12a3d6140ffcc1e478acd2bef8148131d9689736
Adrian Andrei <aandrei@uniphar.ie>
2025-11-03 15:02:11 +0200

    qqq

--- OrleansPlayground/Program.cs
+++ OrleansPlayground/Program.cs
@@ -30,8 +30,8 @@ builder.Host.UseOrleans(silo =>
 
     silo.Configure<GrainCollectionOptions>(o =>
     {
-        o.CollectionAge = TimeSpan.FromSeconds(10);
-        o.CollectionQuantum = TimeSpan.FromSeconds(9);
+        o.CollectionAge = TimeSpan.FromSeconds(30);
+        o.CollectionQuantum = TimeSpan.FromSeconds(10);
     });
 
     silo.Configure<MessagingOptions>(options =>
