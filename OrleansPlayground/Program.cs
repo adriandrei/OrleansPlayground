62723801d610bda8cf44660580e2c37c8b8c0802
Adrian Andrei <aandrei@uniphar.ie>
2025-11-03 13:22:44 +0200

    short collection

--- OrleansPlayground/Program.cs
+++ OrleansPlayground/Program.cs
@@ -30,8 +30,8 @@ builder.Host.UseOrleans(silo =>
 
     silo.Configure<GrainCollectionOptions>(o =>
     {
-        o.CollectionAge = TimeSpan.FromSeconds(60);
-        o.CollectionQuantum = TimeSpan.FromSeconds(59);
+        o.CollectionAge = TimeSpan.FromSeconds(10);
+        o.CollectionQuantum = TimeSpan.FromSeconds(9);
     });
 
     silo.Configure<MessagingOptions>(options =>
